e07002682df571449bc2631f9d2f0e9b8654db31
omaroualha <oualhaomar@gmail.com>
2018-12-09 15:41:36 +0100

    Update Fibonacci.js

--- routes/Fibonacci.js
+++ routes/Fibonacci.js
@@ -2,36 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 
-
 router.get("/evenValue", function (err, res) {
-
-      res.send(getFiboncciSequence());
-  
-    }) ;
-
-  
+    res.send(getFiboncciSequence());
+});
 
 
-function getNextValue(sequence)
-{
+function getNextValue(sequence) {
     var n = sequence.length;
-    return sequence[n-1] + sequence[n-2];
+    return sequence[n - 1] + sequence[n - 2];
 }
 
-function getFiboncciSequence()
-{
-    var sequence= [1,2];
+
+function getFiboncciSequence() {
+    var sequence = [1, 2];
     var sum = 0;
 
-    while(sequence[sequence.length-1] < 4000000 )
-    {
+    while (sequence[sequence.length - 1] < 4000000) {
         sequence.push(getNextValue(sequence));
     }
-    sequence.forEach(function(n)
-    {
-        if(n%2===0)
-        {
-            sum +=n;
+    sequence.forEach(function (n) {
+        if (n % 2 === 0) {
+            sum += n;
         }
     });
 
